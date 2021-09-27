3cd91eaff5ec7255d3f11ce46fa7c5e90f087336
Artem Chumak <artem.4x@gmail.com>
2021-09-27 15:40:20 +0300

    mongoose connect error fixed

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const express = require('express'); // подключили экспрес
 const mongoose = require('mongoose'); // подключили mongoose для работы с Mongod
 
 const { PORT = 3000 } = process.env; // указали порт с дефолтным значением 3000
-
-mongoose.connect('mongodb://localhost:27017/mestodb', { // подключили БД. Не забыть, что перед запуском нужно включить БД в терминале mongod
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
 const app = express(); // включили экспресс
 
-app.listen(PORT, () => { // запустили экспресс
-  console.log(`App listening on port ${PORT}`);
-});
+async function start () {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //! не запускается с этими параметрами, ругается, хотя они были в уроке
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    });
+    app.listen(PORT, () => console.log(`App listining on port ${PORT}`));
+  } catch (e) {
+    console.log('Server error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,20 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ users }))
-    .catch((e) => res.status(500).send({ message: `Произшла ошибка ${e}` }));
+    .catch((e) => res.status(500).send({ message: `Произшла ошибка ${e.message}` }));
 };
 
 const getCurrentUser = (req, res) => {
   User.findById(req.user._id)
     .then((user) => res.status(200).send({ data: user }))
-    .catch((e) => res.status(500).send({ message: `Произошла ошибка ${e}` }));
+    .catch((e) => res.status(500).send({ message: `Произошла ошибка ${e.message}` }));
 };
 
 const creatUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.creat({ name, about, avatar })
     .then((user) => res.status(200).send({ data: user }))
-    .catch((e) => res.status(200).send({ message: `роизошла ошибка ${e}` }));
+    .catch((e) => res.status(200).send({ message: `роизошла ошибка ${e.message}` }));
 };
 
 module.exports = {
